7f80bf620e1d82aa2c0ccf462fd892a30c099e8f
collins-kimotho <collinskimotho16@gmail.com>
2024-10-07 03:54:36 -0700

    Iterate through object

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,8 +1,6 @@
 export default function iterateThroughObject(reportWithIterator) {
   // Convert iterator into an array of employees
   const employees = [...reportWithIterator];
-  
   // Join the array into a single string, separated by '|'
   return employees.join(' | ');
 }
-
