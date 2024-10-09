1595d8123eaea180790d6dd7402b67ba05138060
collins-kimotho <collinskimotho16@gmail.com>
2024-10-09 05:13:52 -0700

    pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,46 @@
+import Currency from './3-currency';
+
+class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;   // This will invoke the setter
+    this.currency = currency; // This will invoke the setter
+  }
+
+  set amount(amount) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = amount;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set currency(currency) {
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
+    this._currency = currency;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    if (typeof conversionRate !== 'number') {
+      throw new TypeError('Conversion rate must be a number');
+    }
+    return amount * conversionRate;
+  }
+}
+
+export default Pricing;
