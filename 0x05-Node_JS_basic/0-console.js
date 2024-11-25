b772574fd4b080b01bea9696800b49a5f3cd70bf
collins-kimotho <collinskimotho16@gmail.com>
2024-11-25 23:12:14 +0300

    first commit

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,7 @@
+// Function to print a message to STDOUT
+function displayMessage(message) {
+    console.log(message); 
+}
+
+// Export the function using module.exports
+module.exports = displayMessage;
