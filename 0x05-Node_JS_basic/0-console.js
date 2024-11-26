a823f6cec5909242febcc8df05cfbe5a03c60dcd
collins-kimotho <collinskimotho16@gmail.com>
2024-11-26 05:35:00 +0300

    arrow function

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,7 +1,10 @@
-// Function to print a message to STDOUT
-
-const displayMessage = (message) => {
-    console.log(message)
-}
-// Export the function using module.exports
-module.exports = displayMessage;
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ */
+const displayMessage = (msg) => {
+    console.log(msg);
+  };
+  
+  module.exports = displayMessage;
